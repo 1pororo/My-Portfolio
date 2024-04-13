e23ca21d98e0e7c13ecede1fca1ba67e45d53584
Ash <hashiere29@gmail.com>
2024-04-12 22:53:27 -0400

    Creating navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Home from "./Home";
+import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar flex  max-w-4xl mx-auto my-0 p-3 text-base items-center">
+      <div className="branding">
+        <h1 className="font-bold text-lg">Portfolio</h1>
+      </div>
+      <div className="links ml-auto">
+        <Link to="/">Home</Link>
+        <Link to="/">Projects</Link>
+        <Link to="/">Contact</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
